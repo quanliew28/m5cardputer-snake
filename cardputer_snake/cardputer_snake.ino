3ecddb6f3e0a653bc0879b6d626cce1dd5f8e2cf
Darren Liew <darrenliew96@gmail.com>
2025-10-30 00:56:25 +0800

    updated arduino version of the code

--- cardputer_snake/cardputer_snake.ino
+++ cardputer_snake/cardputer_snake.ino
@@ -2,7 +2,6 @@
 
 M5Canvas canvas(&M5Cardputer.Display);
 
-
 const int8_t GRID_SIZE = 4;
 
 int8_t snake[256][2];
@@ -13,12 +12,13 @@ int16_t snakeLength = 1;
 int8_t appleX;
 int8_t appleY;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   auto cfg = M5.config();
-  M5Cardputer.begin(cfg, true);  // enableKeyboard
+  M5Cardputer.begin(cfg, true); // enableKeyboard
 
-  //Init Display canvas
+  // Init Display canvas
   uint8_t displayWidth = M5Cardputer.Display.width();
   uint8_t displayHeight = M5Cardputer.Display.height();
 
@@ -32,60 +32,87 @@ void setup() {
   spawnApple();
 }
 
-void loop() {
+void loop()
+{
   M5Cardputer.update();
   tick();
   render();
 }
 
-void tick() {
+void tick()
+{
 
-  if (M5Cardputer.Keyboard.isKeyPressed(';') && snakeDirection != 2) {
+  if (M5Cardputer.Keyboard.isKeyPressed(';') && snakeDirection != 2)
+  {
     snakeDirection = 0;
-  } else if (M5Cardputer.Keyboard.isKeyPressed('/') && snakeDirection != 3) {
+  }
+  else if (M5Cardputer.Keyboard.isKeyPressed('/') && snakeDirection != 3)
+  {
     snakeDirection = 1;
-  } else if (M5Cardputer.Keyboard.isKeyPressed('.') && snakeDirection != 0) {
+  }
+  else if (M5Cardputer.Keyboard.isKeyPressed('.') && snakeDirection != 0)
+  {
     snakeDirection = 2;
-  } else if (M5Cardputer.Keyboard.isKeyPressed(',') && snakeDirection != 1) {
+  }
+  else if (M5Cardputer.Keyboard.isKeyPressed(',') && snakeDirection != 1)
+  {
     snakeDirection = 3;
   }
 
   snakeSpeed++;
-  if (snakeSpeed >= 100) {
+  if (snakeSpeed >= 100)
+  {
 
-    for (int i = snakeLength; i > 0; i--) {
+    for (int i = snakeLength; i > 0; i--)
+    {
       snake[i][0] = snake[i - 1][0];
       snake[i][1] = snake[i - 1][1];
     }
-    switch (snakeDirection) {
-      case 0:
-        snake[0][1]--;
-        if (snake[0][1] == -1) {
-          snake[0][1] = 33;
-        }
-        break;
-      case 1:
-        snake[0][0]++;
-        if (snake[0][0] == 60) {
-          snake[0][0] = 0;
-        }
-        break;
-      case 2:
-        snake[0][1]++;
-        if (snake[0][1] == 34) {
-          snake[0][1] = 0;
-        }
-        break;
-      case 3:
-        snake[0][0]--;
-        if (snake[0][0] == -1) {
-          snake[0][0] = 59;
-        }
-        break;
+    switch (snakeDirection)
+    {
+    case 0:
+      snake[0][1]--;
+      if (snake[0][1] == -1)
+      {
+        snake[0][1] = 33;
+      }
+      break;
+    case 1:
+      snake[0][0]++;
+      if (snake[0][0] == 60)
+      {
+        snake[0][0] = 0;
+      }
+      break;
+    case 2:
+      snake[0][1]++;
+      if (snake[0][1] == 34)
+      {
+        snake[0][1] = 0;
+      }
+      break;
+    case 3:
+      snake[0][0]--;
+      if (snake[0][0] == -1)
+      {
+        snake[0][0] = 59;
+      }
+      break;
     }
-    if (snake[0][0] == appleX && snake[0][1] == appleY) {
+    for (int i = 1; i < snakeLength; i++)
+    {
+      if (snake[0][0] == snake[i][0] && snake[0][1] == snake[i][1])
+      {
+        resetGame();
+        return;
+      }
+    }
+
+    if (snake[0][0] == appleX && snake[0][1] == appleY)
+    {
       // grow snake by one (prevent exceeding buffer)
-      if (snakeLength < 255) {
+      if (snakeLength < 255)
+      {
         snakeLength++;
       }
       spawnApple();
@@ -95,8 +122,10 @@ void tick() {
   }
 }
 
-void spawnApple() {
-  do {
+void spawnApple()
+{
+  do
+  {
     appleX = random(0, 60);
     appleY = random(0, 34);
     Serial.println(appleX);
@@ -104,20 +133,34 @@ void spawnApple() {
   } while (checkApple());
 }
 
-bool checkApple() {
+bool checkApple()
+{
   bool foundError = false;
-  for (int i = 0; i < snakeLength; i++) {
-    if (snake[0][0] == appleX && snake[0][1] == appleY) {
+  for (int i = 0; i < snakeLength; i++)
+  {
+    if (snake[0][0] == appleX && snake[0][1] == appleY)
+    {
       foundError = true;
     }
   }
   return foundError;
 }
 
+void resetGame()
+{
+  snakeLength = 1;
+  snakeDirection = 0;
+  snake[0][0] = 2;
+  snake[0][1] = 2;
+  spawnApple();
+}
 
-void render() {
+
+void render()
+{
   canvas.fillScreen(BLACK);
-  for (int i = 0; i < snakeLength; i++) {
+  for (int i = 0; i < snakeLength; i++)
+  {
     canvas.fillRect(snake[i][0] * GRID_SIZE, snake[i][1] * GRID_SIZE, 4, 4, WHITE);
   }
   canvas.fillRect(appleX * GRID_SIZE, appleY * GRID_SIZE, 4, 4, RED);
